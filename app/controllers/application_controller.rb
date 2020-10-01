8c916c11c121a1a853b6ace45ffe758b5835ece1
appleRui <apple31581@gmail.com>
2020-10-01 14:25:02 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-<<<<<<< HEAD
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password ,:password_confirmation])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password ,:remember_me])
-=======
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :remember_me])
->>>>>>> 36acdd735d694584b2463fafa8e58cb3f3433a32
   end
 
   def after_sign_in_path_for(resource)
