0f4d43a4b722781cb440ced45e6fb797b6cb1f2f
appleRui <apple31581@gmail.com>
2020-10-01 14:12:43 +0900

    login update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable
+         :recoverable, :rememberable, :validatable, :timeoutable, :authentication_keys => [:login]
 
          has_many :lists, dependent: :destroy
          validates :name, presence: true
+         attr_accessor :login
+
+         def self.find_first_by_auth_conditions(warden_conditions)
+          conditions = warden_conditions.dup
+          if login = conditions.delete(:login)
+            where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }]).first
+          else
+            where(conditions).first
+          end
+        end
+
 end
