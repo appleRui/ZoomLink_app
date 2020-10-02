e005ba2310ade28ba416333fe89c666489fa2157
appleRui <apple31581@gmail.com>
2020-10-02 18:48:54 +0900

    0.0.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :user ,only: [:edit, :update]
+  resources :user ,only: [:update]
   resources :lists
   root to: 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
